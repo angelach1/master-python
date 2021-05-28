9f9e5273484093e210c9fc741fdaa1637460078c
angelach1 <angel.alhambra@gmail.com>
2021-05-28 12:26:27 +0200

    Sección 8 Capítulo 38. Crear funciones

---
+++ 08-funciones/main.py
@@ -0,0 +1,31 @@
+"""
+FUNCIONES
+Una función es un conjunto de instrucciones agrupadas bajo un nombre
+concreto que pueden reutilizarse invocando a la función tantas veces
+como sea necesario.
+
+Declaración:
+
+def nombreDeMiFuncion(parametros):
+    #BLOQUE / CONJUNTO DE INSTRUCCIONES
+
+Uso:
+
+nombreDeMiFuncion(mi_parametro, ...)
+"""
+
+#Ejemplo 1
+print ("######## EJEMPLO1 #######")
+
+# Definición de la función
+def muestraNombre():
+    print("Ángel")
+    print("Pepe")
+    print("Indalecio")
+    print("Loli")
+    print("\n")
+
+#Invocar a la fución
+muestraNombre()
+muestraNombre()
+
