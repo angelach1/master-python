fae18ac9d15d43eab9b58f38c74a1f6eb22bbf31
angelach1 <angel.alhambra@gmail.com>
2021-07-12 11:18:54 +0200

    63. Introducción a los módulos

---
+++ 12-modulos/main.py
@@ -0,0 +1,9 @@
+"""
+Módulos: Son funcionalidades ya hechas para reutilizar (librerías).
+
+Los módulos se pueden consultar en:
+    https://docs.python.org/3/py-modindex.html
+
+Podemos conseguir módulos que ya vienen en el lenguaje, módulos en internet, y tambíen
+podemos crear nuestros propios módulos.
+"""
