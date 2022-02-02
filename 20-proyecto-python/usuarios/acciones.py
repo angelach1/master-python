d07ca992f772c25fa61f75aad587ec144061e79d
angelach1 <angel.alhambra@gmail.com>
2022-02-02 10:33:58 +0100

    111 - Capturar errores en el registro de usuarios

--- 20-proyecto-python/usuarios/acciones.py
+++ 20-proyecto-python/usuarios/acciones.py
@@ -1,3 +1,5 @@
+from usuarios import usuario as modelo
+
 class Acciones:
 
     def registro(self):
@@ -7,8 +9,17 @@ class Acciones:
         email = input("Introduce tu email: ")
         password = input("Introduce tu contraseña: ")
 
+        usuario = modelo.Usuario(nombre, apellidos, email, password)
+        registro = usuario.registrar()
+
+        if registro[0] >= 1:
+            print(f"\nPerfecto {registro[1].nombre} te has registrado con el email {registro[1].email}")
+        
+        else:
+            print("\nNo te has registrado correctamente.")
+
     def login(self):
-        print("\n¡¡Vale!! identifícate enel sistema...")
+        print("\n¡¡Vale!! identifícate en el sistema...")
         email = input("Introduce tu email: ")
         password = input("Introduce tu contraseña: ")
 
